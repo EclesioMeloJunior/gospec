a999ac21ab1948cc549e1b05ad94095895f74612
eclesiomelojunior <eclesiomelo.1@gmail.com>
2020-11-09 13:00:26 -0300

    chore: parsing file and implementing log output

--- gospec.go
+++ gospec.go
@@ -1,22 +1,40 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/url"
+	"strings"
+	"time"
+
+	"gopkg.in/yaml.v2"
+)
+
+// testoutput standartize [10-10-2020] Test http://localhost:8000/endpoint: OK
+const testoutput = `[%s] Test %s: %s`
+
+type testexpected struct {
+	Headers []string               `yaml:"headers"`
+	Status  int                    `yaml:"status"`
+	Body    map[string]interface{} `yaml:"body"`
+}
 
 type testendpoints struct {
-	Path   string
-	Method string
+	Path   string `yaml:"path"`
+	Method string `yaml:"method"`
 }
 
 type testsection struct {
-	Port      int
-	Host      string
-	Protocol  string
-	Endpoints []testendpoints
+	Port      int             `yaml:"port"`
+	Host      string          `yaml:"host"`
+	Protocol  string          `yaml:"protocol"`
+	Endpoints []testendpoints `yaml:"endpoints"`
 }
 
 type specfile struct {
-	Version int
-	Testing []testsection
+	Version int           `yaml:"version"`
+	Testing []testsection `yaml:"testing"`
 }
 
 func (s *specfile) isVersionValid() bool {
@@ -28,9 +46,61 @@ func gospec(testfile string) error {
 		return errors.New("Test file could not be empty")
 	}
 
+	spec, err := parseFile(testfile)
+
+	if err != nil {
+		return err
+	}
+
+	suite(spec)
+
 	return nil
 }
 
-func parseFile(testfile string) *specfile {
-	return &specfile{}
+func parseFile(testfile string) (*specfile, error) {
+	var err error
+	var filebytes []byte
+
+	if filebytes, err = ioutil.ReadFile(testfile); err != nil {
+		return nil, err
+	}
+
+	s := new(specfile)
+	err = yaml.Unmarshal(filebytes, s)
+
+	return s, err
+}
+
+func suite(s *specfile) error {
+	if !s.isVersionValid() {
+		return errors.New("Test file version is not valid")
+	}
+
+	for _, testing := range s.Testing {
+		baseURL := buildURL(testing)
+
+		for _, endpoint := range testing.Endpoints {
+			if strings.HasPrefix(endpoint.Path, "/") {
+				endpoint.Path = string(endpoint.Path[1:])
+			}
+
+			testResult := "OK"
+			printTestResult(baseURL.String(), testResult)
+		}
+	}
+
+	return nil
+}
+
+func buildURL(t testsection) url.URL {
+	return url.URL{
+		Scheme: t.Protocol,
+		Host:   fmt.Sprintf(`%s:%v`, t.Host, t.Port),
+	}
+}
+
+func printTestResult(url string, result string) {
+	testTime := time.Now().Format(time.RFC822Z)
+	output := fmt.Sprintf(testoutput, testTime, url, result)
+	fmt.Println(output)
 }
